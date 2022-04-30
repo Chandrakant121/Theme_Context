915e573a574579eaaa3555d17c950b5028e2120f
Chandrakant121 <csjoshi989@gmail.com>
2022-04-30 16:07:30 +0530

    ThemeContext Assignment

---
+++ src/components/ThemeButton.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+
+const LightTheme = styled.div`
+background: white;
+`;
+
+const DarkTheme = styled.div`
+height: 450px;
+width: 100%;
+background:black;
+`;
+
+export { DarkTheme, LightTheme }
